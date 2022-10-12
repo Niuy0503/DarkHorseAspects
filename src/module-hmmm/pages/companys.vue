da1d80da7d054f0a4d65de9ba7c0b0d412bae39c
xiaoyegecode <1010039036@qq.com>
2022-10-12 21:30:23 +0800

    form结构完成

--- src/module-hmmm/pages/companys.vue
+++ src/module-hmmm/pages/companys.vue
@@ -1,6 +1,51 @@
 <template>
-  <div>
-    企业管理
+  <div class="app-container">
+    <el-card shadow="never">
+      <!-- form表单 -->
+      <el-form label-width="80px">
+        <el-row>
+          <el-col :span="6">
+            <el-form-item label="标签名称">
+              <el-input placeholder="请输入"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="城市">
+              <el-input placeholder="请输入"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="地区">
+              <el-input placeholder="请输入"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="企业简称">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="状态">
+              <el-input placeholder="请输入"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6" style="padding-left:40px">
+            <el-button type="default" size="small">清除</el-button>
+            <el-button type="primary" size="small">搜索</el-button>
+          </el-col>
+          <el-col :span="12" style="text-align:right">
+            <el-button type="success" size="small" icon="el-icon-edit">新增用户</el-button>
+          </el-col>
+        </el-row>
+      </el-form>
+      <!-- 提示 -->
+      <el-alert :title="title" type="info" show-icon></el-alert>
+      <!-- 表格 -->
+      <el-table></el-table>
+      <!-- 分页器 -->
+      <el-pagination></el-pagination>
+    </el-card>
+
   </div>
 </template>
 
@@ -8,7 +53,7 @@
 export default {
   data () {
     return {
-
+      total: 11
     }
   },
 
@@ -18,10 +63,17 @@ export default {
 
   methods: {
 
+  },
+  computed: {
+    title () {
+      return `共${this.total}条记录`
+    }
   }
 }
 </script>
 
 <style scoped lang='less'>
-
+.app-container {
+  padding: 20px;
+}
 </style>
